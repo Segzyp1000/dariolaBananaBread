20254dffd83c51adc6528dacd76a42298f5bcbb5
segun olowoyeye <segunolowoyeye@gmail.com>
2024-06-13 02:52:24 +0100

    fourth commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -20,7 +20,9 @@ const Menu = () => {
             <img src={Oreos} alt="" className="rounded-full p-6" />
             <p className="flex justify-start">Oreos Banana Bread</p>
             <p className="font-bold">N14,000</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
         <div className="bg-white shadow-lg text-black p-3  border-2 border-slate-200 rounded-lg">
@@ -28,7 +30,9 @@ const Menu = () => {
             <img src={ChocolateChip} alt="" className="rounded-full p-6" />
             <p>Chocolate Chips Banana Bread</p>
             <p className="font-bold">N12,500</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
         <div className="bg-white shadow-lg  text-black p-3  border-2 border-slate-200 rounded-lg">
@@ -36,7 +40,9 @@ const Menu = () => {
             <img src={Plain} alt="" className="rounded-full p-6" />
             <p>Plain Banana Bread</p>
             <p className="font-bold">N10,000</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
         <div className="bg-white shadow-lg  text-black p-3  border-2 border-slate-200 rounded-lg">
@@ -44,7 +50,9 @@ const Menu = () => {
             <img src={Mixed} alt="" className="rounded-full p-6" />
             <p>Mixed Nuts Banana Bread</p>
             <p className="font-bold">N11,000</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
         <div className="bg-white shadow-lg  text-black p-3  border-2 border-slate-200rounded-lg">
@@ -52,7 +60,9 @@ const Menu = () => {
             <img src={Raisin} alt="" className="rounded-full p-6" />
             <p>Raisin Banana Bread</p>
             <p className="font-bold">N9,000</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
         <div className="bg-white shadow-lg  text-black p-3  border-2 border-slate-200 rounded-lg">
@@ -60,7 +70,9 @@ const Menu = () => {
             <img src={Marble} alt="" className="rounded-full p-6" />
             <p>Marble Banana Bread</p>
             <p className="font-bold">N24,000</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
         <div className="bg-white shadow-lg  text-black p-3 border-2 border-slate-200 rounded-lg">
@@ -68,7 +80,9 @@ const Menu = () => {
             <img src={Double} alt="" className="rounded-full p-6" />
             <p>Double chocolate Banana Bread</p>
             <p className="font-bold">N18,000</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
         <div className="bg-white shadow-lg  text-black p-3  border-2 border-slate-200 rounded-lg">
@@ -76,7 +90,9 @@ const Menu = () => {
             <img src={Cashew} alt="" className="rounded-full p-6" />
             <p>Cashew Banana Bread</p>
             <p className="font-bold">N14,000</p>
-            <button className="bg-navColor p-3 rounded-lg">Add +</button>
+            <button className="bg-navColor text-white  p-3 rounded-lg">
+              Add +
+            </button>
           </span>
         </div>
       </div>
